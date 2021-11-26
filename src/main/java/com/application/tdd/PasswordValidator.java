d9c52f45ecc60384f9141ad18d67b97597cfffd9
alex-omeara <72926811+alex-omeara@users.noreply.github.com>
2021-11-26 19:26:53 +0000

    Added PasswordValidator class, and failing test to check password input is at least 7 characters in length

---
+++ src/main/java/com/application/tdd/PasswordValidator.java
@@ -0,0 +1,7 @@
+package com.application.tdd;
+
+public class PasswordValidator {
+    public boolean checkLength(String password) {
+        return false;
+    }
+}
--- src/test/java/com/application/tdd/PasswordInputTest.java
+++ src/test/java/com/application/tdd/PasswordInputTest.java
@@ -1,4 +1,20 @@
 package com.application.tdd;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PasswordInputTest {
+    PasswordValidator passwordValidator;
+
+    @BeforeEach
+    public void setUp() {
+        passwordValidator = new PasswordValidator();
+    }
+
+    @Test
+    public void testPasswordAtLeastSevenCharacters() {
+        Assertions.assertTrue(passwordValidator.checkLength("password"));
+    }
+
 }
