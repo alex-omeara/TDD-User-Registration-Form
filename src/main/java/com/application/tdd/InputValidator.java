10a362e08a1a150445934847d7cdf9943da973b5
alex-omeara <72926811+alex-omeara@users.noreply.github.com>
2021-11-26 17:35:23 +0000

    Updated code to pass test to check if email input has @ symbol,

--- src/main/java/com/application/tdd/InputValidator.java
+++ src/main/java/com/application/tdd/InputValidator.java
@@ -2,6 +2,12 @@ package com.application.tdd;
 
 public class InputValidator {
     public boolean validateInput(String input) {
-        return input.isEmpty();
+        boolean isValid = false;
+        if (input.isEmpty()) {
+            isValid = true;
+        } else if (input.contains("@")) {
+            isValid = true;
+        }
+        return isValid;
     }
 }
