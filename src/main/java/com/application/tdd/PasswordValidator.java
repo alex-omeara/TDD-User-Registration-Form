a9182774c435898a56e032c2673415607b2cdbeb
alex-omeara <72926811+alex-omeara@users.noreply.github.com>
2021-11-26 19:27:44 +0000

    Added code to pass tests checking if password input at least 7 characters in length

--- src/main/java/com/application/tdd/PasswordValidator.java
+++ src/main/java/com/application/tdd/PasswordValidator.java
@@ -2,6 +2,6 @@ package com.application.tdd;
 
 public class PasswordValidator {
     public boolean checkLength(String password) {
-        return false;
+        return password.length() >= 7;
     }
 }
